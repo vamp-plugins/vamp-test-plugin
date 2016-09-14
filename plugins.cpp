7961f3406a694fc14de3e203d48b4934c82bf28c
Chris Cannam <c.cannam@qmul.ac.uk>
2016-09-14 17:56:19 +0100

    Copyright note, untabify

--- plugins.cpp
+++ plugins.cpp
@@ -1,4 +1,32 @@
+/* -*- c-basic-offset: 4 indent-tabs-mode: nil -*-  vi:set ts=8 sts=4 sw=4: */
+/*
+    Vamp Test Plugin
+    Copyright (c) 2013-2016 Queen Mary, University of London
 
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use, copy,
+    modify, merge, publish, distribute, sublicense, and/or sell copies
+    of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+    CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    Except as contained in this notice, the names of the Centre for
+    Digital Music and Queen Mary, University of London shall not be
+    used in advertising or otherwise to promote the sale, use or other
+    dealings in this Software without prior written authorization.
+*/
 
 #include <vamp/vamp.h>
 #include <vamp-sdk/PluginAdapter.h>
@@ -9,8 +37,8 @@ class Adapter : public Vamp::PluginAdapterBase
 {
 public:
     Adapter(bool freq) :
-	PluginAdapterBase(),
-	m_freq(freq) { }
+        PluginAdapterBase(),
+        m_freq(freq) { }
 
     virtual ~Adapter() { }
 
@@ -18,7 +46,7 @@ protected:
     bool m_freq;
 
     Vamp::Plugin *createPlugin(float inputSampleRate) {
-	return new VampTestPlugin(inputSampleRate, m_freq);
+        return new VampTestPlugin(inputSampleRate, m_freq);
     }
 };
 
